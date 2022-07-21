c29874b15fd134cc86d8a51a16056a11c1b1f007
Anderson Arruda <andmarruda@gmail.com>
2022-07-21 19:24:02 -0300

    Create exampleStrBindNum.php

---
+++ examples/exampleStrBindNum.php
@@ -0,0 +1,8 @@
+<?php
+  require_once(__DIR__. '/../strUtil.class.php');
+  use \sysbrog\strUtil;
+  $string = 'SELECT * FROM teste WHERE teste=? AND teste2=? AND teste IN (?, ?, ?, ?)';
+
+  $binded = strUtil::strBindNum($string, 'abc', 'cde', 1, 2, 3, 4);
+  var_dump($binded); //SELECT * FROM teste WHERE teste=abc AND teste2=cde AND teste IN (1, 2, 3, 4)
+?>
